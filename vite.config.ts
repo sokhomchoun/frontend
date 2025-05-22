a430305e8c052bbe74c95260c334e53e87a73bdc
DENZ\choun <chounsokhom2002@gmail.com>
2025-05-22 10:14:06 +0700

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-    plugins: [vue()],
-    base: '/frontend/',
-    resolve: {
-        alias: {
-            vuex: 'vuex/dist/vuex.esm-bundler.js'
-        }
-    },
+// export default defineConfig({
+//     plugins: [vue()],
+//     base: "/",
+//     resolve: {
+//         alias: {
+//             vuex: 'vuex/dist/vuex.esm-bundler.js'
+//         }
+//     },
     
-})
+// })
+
+
+export default defineConfig(({ mode }) => ({
+    plugins: [vue()],
+    base: mode === 'production' ? '/frontend/' : '/',
+    resolve: {
+            alias: {
+                vuex: 'vuex/dist/vuex.esm-bundler.js'
+            }
+        },
+}))
